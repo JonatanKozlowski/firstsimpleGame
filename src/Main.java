05dcf6c6e423a9c3f6907c9073f6f5846f611615
Jonatan Kozlowski <kozlowskijonatan@gmail.com>
2023-01-08 00:43:27 +0100

    Final version!!! (im done but, idk that I don't upgrade this)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("jflkasklfdjsaf");
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj! Grasz teraz w grę o nazawie: 'Find a number'.");
+        System.out.println("Zasady gry są proste, Aby wygrać musisz odgadnąć liczbę wylosowaną przez komputer, gdy wpiszesz " +
+                "jaką kolwiek liczbę program mówi czy wylosowana liczba jest wieksza czy mniejsza. ");
+        boolean itsTrue = true;
+        while (itsTrue) {
+            System.out.print("Wpisz liczbę:");
+            int number = scanner.nextInt();
+            if (number != numberToGuess && number < numberToGuess){
+                System.out.println("Wylosowana liczba jest większa"+" ↑");
+            } else if (number != numberToGuess && number > numberToGuess) {
+                System.out.println("Wylosowana liczba jest mniejsza"+" ↓");
+            }else if (number == numberToGuess){
+                System.out.println("Gratuluję wygrałeś! wylosowana liczba to: " + numberToGuess);
+                break;
+            }
+        }
     }
 }
